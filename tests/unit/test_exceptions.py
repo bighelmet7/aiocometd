b760810a7af3a786b35fde3b4136cf364eaf2a3f
Abner Andino <brain.agam@gmail.com>
2024-10-22 00:18:16 +0100

    refactor test_exceptions with pytest

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -1,89 +1,98 @@
-from unittest import TestCase, mock
+from unittest import mock
+
+import pytest
 
 from aiocometd.exceptions import ServerError
 
 
-class TestServerError(TestCase):
-    def test_properties(self):
-        response = {
-            "channel": "/meta/subscription",
-            "successful": False,
-            "id": "0",
-            "error": "error"
-        }
+@pytest.fixture
+def mock_utils():
+    with mock.patch("aiocometd.exceptions.utils") as mock_func:
+        yield mock_func
 
-        error = ServerError("description message", response)
 
-        self.assertEqual(error.message, "description message")
-        self.assertEqual(error.response, response)
-        self.assertEqual(error.error, "error")
+def test_properties():
+    response = {
+        "channel": "/meta/subscription",
+        "successful": False,
+        "id": "0",
+        "error": "error",
+    }
 
-    def test_properties_on_no_error(self):
-        response = {
-            "channel": "/meta/subscription",
-            "successful": False,
-            "id": "0"
-        }
+    error = ServerError("description message", response)
 
-        error = ServerError("description message", response)
+    assert error.message == "description message"
+    assert error.response == response
+    assert error.error == "error"
 
-        self.assertEqual(error.message, "description message")
-        self.assertEqual(error.response, response)
-        self.assertIsNone(error.error)
 
-    def test_properties_on_no_response(self):
-        response = None
+def test_properties_on_no_error():
+    response = {
+        "channel": "/meta/subscription",
+        "successful": False,
+        "id": "0",
+    }
 
-        error = ServerError("description message", response)
+    error = ServerError("description message", response)
 
-        self.assertEqual(error.message, "description message")
-        self.assertIsNone(error.response)
-        self.assertIsNone(error.error)
+    assert error.message == "description message"
+    assert error.response == response
+    assert error.error is None
 
-    @mock.patch("aiocometd.exceptions.utils")
-    def test_error_code(self, utils):
-        response = {
-            "channel": "/meta/subscription",
-            "successful": False,
-            "id": "0",
-            "error": "error"
-        }
-        utils.get_error_code.return_value = 12
 
-        error = ServerError("description message", response)
-        result = error.error_code
+def test_properties_on_no_response():
+    response = None
 
-        self.assertEqual(result, utils.get_error_code.return_value)
-        utils.get_error_code.assert_called_with(response["error"])
+    error = ServerError("description message", response)
 
-    @mock.patch("aiocometd.exceptions.utils")
-    def test_error_message(self, utils):
-        response = {
-            "channel": "/meta/subscription",
-            "successful": False,
-            "id": "0",
-            "error": "error"
-        }
-        utils.get_error_message.return_value = "message"
+    assert error.message == "description message"
+    assert error.response is None
+    assert error.error is None
 
-        error = ServerError("description message", response)
-        result = error.error_message
 
-        self.assertEqual(result, utils.get_error_message.return_value)
-        utils.get_error_message.assert_called_with(response["error"])
+def test_error_code(mock_utils):
+    response = {
+        "channel": "/meta/subscription",
+        "successful": False,
+        "id": "0",
+        "error": "error",
+    }
+    mock_utils.get_error_code.return_value = 12
 
-    @mock.patch("aiocometd.exceptions.utils")
-    def test_error_args(self, utils):
-        response = {
-            "channel": "/meta/subscription",
-            "successful": False,
-            "id": "0",
-            "error": "error"
-        }
-        utils.get_error_args.return_value = ["arg"]
+    error = ServerError("description message", response)
+    result = error.error_code
 
-        error = ServerError("description message", response)
-        result = error.error_args
+    assert result == mock_utils.get_error_code.return_value
+    mock_utils.get_error_code.assert_called_with(response["error"])
 
-        self.assertEqual(result, utils.get_error_args.return_value)
-        utils.get_error_args.assert_called_with(response["error"])
+
+def test_error_message(mock_utils):
+    response = {
+        "channel": "/meta/subscription",
+        "successful": False,
+        "id": "0",
+        "error": "error",
+    }
+    mock_utils.get_error_message.return_value = "message"
+
+    error = ServerError("description message", response)
+    result = error.error_message
+
+    assert result == mock_utils.get_error_message.return_value
+    mock_utils.get_error_message.assert_called_with(response["error"])
+
+
+def test_error_args(mock_utils):
+    response = {
+        "channel": "/meta/subscription",
+        "successful": False,
+        "id": "0",
+        "error": "error",
+    }
+    mock_utils.get_error_args.return_value = ["arg"]
+
+    error = ServerError("description message", response)
+    result = error.error_args
+
+    assert result == mock_utils.get_error_args.return_value
+    mock_utils.get_error_args.assert_called_with(response["error"])
